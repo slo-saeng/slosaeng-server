ed97ba1f082c0b50d448c789bfad377a46fe18f5
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-08 15:44:38 +0900

    feat : gradle 초기 세팅

---
+++ src/main/java/com/server/slosaeng/SlosaengApplication.java
@@ -0,0 +1,13 @@
+package com.server.slosaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlosaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlosaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/slosaeng/SlosaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.slosaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlosaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3f15bfa4a5a0dd8dd2c382b9571d2d92666162a4
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 01:32:40 +0900

    feat : SecurityConfig 작성

---
+++ src/main/java/com/server/slosaeng/global/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.server.slosaeng.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.server.slosaeng.domain.member.application.MemberDetailService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+	private final MemberDetailService memberDetailService;
+
+	@Bean
+	public WebSecurityCustomizer configure() {
+		return (web) -> web.ignoring()
+			.requestMatchers(new AntPathRequestMatcher("/static/**"));
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(
+					new AntPathRequestMatcher("/login")
+				).permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(logout -> logout
+				.logoutSuccessUrl("/login")
+				.invalidateHttpSession(true)
+			)
+			.csrf(AbstractHttpConfigurer::disable)
+			.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+		HttpSecurity http,
+		BCryptPasswordEncoder bCryptPasswordEncoder,
+		MemberDetailService memberDetailService
+	) throws Exception {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(memberDetailService);
+		authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+		return new ProviderManager(authProvider);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}

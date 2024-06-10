afb03ea1219a5a4582ee43735ad09e087cbd8ba7
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 01:31:54 +0900

    feat : 멤버 도메인 구현

---
+++ src/main/java/com/server/slosaeng/domain/member/domain/Member.java
@@ -0,0 +1,65 @@
+package com.server.slosaeng.domain.member.domain;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "member")
+@Inheritance(strategy = InheritanceType.JOINED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member implements UserDetails {
+
+	@Id
+	@Column(nullable = false, updatable = false, unique = true)
+	private String id;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String name;
+
+	@CreationTimestamp
+	@Column(name = "joined_at", updatable = false, nullable = false)
+	private LocalDateTime joinedAt;
+
+	@Builder
+	public Member(String id, String password, String name, String auth) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return name;
+	}
+}

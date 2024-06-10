38b1f43aa4046af4bbdb92feb595dc0a0603b291
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 01:32:24 +0900

    feat : jwt 토큰 발행 및 인증

---
+++ src/main/java/com/server/slosaeng/global/config/jwt/TokenProvider.java
@@ -0,0 +1,80 @@
+package com.server.slosaeng.global.config.jwt;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import com.server.slosaeng.domain.member.domain.Member;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TokenProvider {
+	private final JwtProperties jwtProperties;
+
+	public String generateToken(Member member, Duration expiredAt) {
+		Date now = new Date();
+		return makeToken(new Date(now.getTime() + expiredAt.toMillis()), member);
+	}
+
+	private String makeToken(Date expiry, Member member) {
+		Date now = new Date();
+		return Jwts.builder()
+			.setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+			.setIssuer(jwtProperties.getIssuer())
+			.setSubject(member.getId())
+			.setIssuedAt(now)
+			.setExpiration(expiry)
+			.claim("id", member.getId())
+			.signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+			.compact();
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser()
+				.setSigningKey(jwtProperties.getSecretKey())
+				.parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Authentication getAuthentication(String token) {
+		Claims claims = getClaims(token);
+		Set<SimpleGrantedAuthority> authorities = Collections.singleton(
+			new SimpleGrantedAuthority("ROLE_USER")
+		);
+		return new UsernamePasswordAuthenticationToken(
+			new org.springframework.security.core.userdetails.User(
+				claims.getSubject(),
+				"",
+				authorities
+			), token, authorities
+		);
+	}
+
+	public String getMemberId(String token) {
+		Claims claims = getClaims(token);
+		return claims.get("id", String.class);
+	}
+
+	private Claims getClaims(String token) {
+		return Jwts.parser()
+			.setSigningKey(jwtProperties.getSecretKey())
+			.parseClaimsJws(token)
+			.getBody();
+	}
+}

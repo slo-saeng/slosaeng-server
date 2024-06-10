2d8fd771dd3c3b7e5c6aa47eab999d4698db1323
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 01:32:57 +0900

    feat : MemberDetailService 구현

---
+++ src/main/java/com/server/slosaeng/domain/member/application/MemberDetailService.java
@@ -0,0 +1,22 @@
+package com.server.slosaeng.domain.member.application;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.server.slosaeng.domain.member.dao.MemberRepository;
+import com.server.slosaeng.domain.member.domain.Member;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailService implements UserDetailsService {
+	private final MemberRepository memberRepository;
+
+	@Override
+	public Member loadUserByUsername(String username) {
+		return memberRepository.findById(username)
+			.orElseThrow(() -> new UsernameNotFoundException("Member not found"));
+	}
+}

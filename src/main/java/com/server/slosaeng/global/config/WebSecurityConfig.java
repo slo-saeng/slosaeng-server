6e14ec84bf5e8dc0a9d9fd8dbcd0d08edd3fa3db
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 16:35:08 +0900

    refactor: security 수정

--- src/main/java/com/server/slosaeng/global/config/WebSecurityConfig.java
+++ src/main/java/com/server/slosaeng/global/config/WebSecurityConfig.java
@@ -2,18 +2,20 @@ package com.server.slosaeng.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.server.slosaeng.global.auth.application.MemberDetailService;
+import com.server.slosaeng.global.auth.filter.TokenAuthenticationFilter;
+import com.server.slosaeng.global.auth.jwt.TokenProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,43 +23,59 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-	private final MemberDetailService memberDetailService;
+	private final TokenProvider tokenProvider;
 
 	@Bean
 	public WebSecurityCustomizer configure() {
 		return (web) -> web.ignoring()
-			.requestMatchers(new AntPathRequestMatcher("/static/**"));
+			.requestMatchers(
+				new AntPathRequestMatcher("/img/**"),
+				new AntPathRequestMatcher("/css/**"),
+				new AntPathRequestMatcher("/js/**")
+			);
 	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(
-					new AntPathRequestMatcher("/login")
+					new AntPathRequestMatcher("/api/token")
 				).permitAll()
-				.anyRequest().authenticated()
+				.anyRequest().permitAll()
 			)
-			.formLogin(AbstractHttpConfigurer::disable)
-			.logout(logout -> logout
-				.logoutSuccessUrl("/login")
-				.invalidateHttpSession(true)
-			)
-			.csrf(AbstractHttpConfigurer::disable)
+			.exceptionHandling(exceptionHandling -> exceptionHandling
+				.defaultAuthenticationEntryPointFor(
+					new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+					new AntPathRequestMatcher("/api/**")
+				))
 			.build();
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(
-		HttpSecurity http,
-		BCryptPasswordEncoder bCryptPasswordEncoder,
-		MemberDetailService memberDetailService
-	) throws Exception {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(memberDetailService);
-		authProvider.setPasswordEncoder(bCryptPasswordEncoder);
-		return new ProviderManager(authProvider);
+	public TokenAuthenticationFilter tokenAuthenticationFilter() {
+		return new TokenAuthenticationFilter(tokenProvider);
 	}
+
+	// @Bean
+	// public AuthenticationManager authenticationManager(
+	// 	HttpSecurity http,
+	// 	BCryptPasswordEncoder bCryptPasswordEncoder,
+	// 	MemberDetailService memberDetailService
+	// ) throws Exception {
+	// 	DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+	// 	authProvider.setUserDetailsService(memberDetailService);
+	// 	authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+	// 	return new ProviderManager(authProvider);
+	// }
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {

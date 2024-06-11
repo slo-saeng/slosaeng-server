4ea0b7899d847a835f251903f738ece2cb83269e
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-12 01:25:21 +0900

    feat: 토큰 삭제

--- src/main/java/com/server/slosaeng/domain/login/dao/RefreshTokenRepository.java
+++ src/main/java/com/server/slosaeng/domain/login/dao/RefreshTokenRepository.java
@@ -12,4 +12,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 	Optional<RefreshToken> findByMemberId(String memberId);
 
 	Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+	void deleteByMemberId(String memberId);
 }

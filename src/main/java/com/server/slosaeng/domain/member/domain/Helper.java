f33fb5b4388ddc8880bde67a23a3af69b90c98f0
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-12 00:55:27 +0900

    feat: Helper 상속 엔티티 설계

---
+++ src/main/java/com/server/slosaeng/domain/member/domain/Helper.java
@@ -0,0 +1,31 @@
+package com.server.slosaeng.domain.member.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@SuperBuilder
+@Table(name = "helper")
+@DiscriminatorValue("HELPER")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Helper extends Member {
+
+	@Column(nullable = false)
+	private String phone;
+
+	@Column(nullable = false)
+	private String idNumber;
+
+	@ElementCollection
+	private List<Long> elderIds;
+}

e257d21ef78ee918ab8b2ff3b0137c3be9dd8e87
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-12 01:25:37 +0900

    feat: Helper 수정 조회 삭제 api

--- src/main/java/com/server/slosaeng/domain/member/api/HelperController.java
+++ src/main/java/com/server/slosaeng/domain/member/api/HelperController.java
@@ -1,5 +1,9 @@
 package com.server.slosaeng.domain.member.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,7 @@ import com.server.slosaeng.domain.member.application.HelperService;
 import com.server.slosaeng.domain.member.dto.request.HelperRequestDto;
 import com.server.slosaeng.global.common.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -20,11 +25,39 @@ public class HelperController {
 	private final HelperService helperService;
 
 	@PostMapping("")
+	@Operation(summary = "보호자 가입")
 	public ApiResponse<?> createHelper(
 		@RequestBody HelperRequestDto helperRequestDto
 	) {
 		String id = helperService.save(helperRequestDto);
 		return ApiResponse.success(id, "보호자 가입 성공");
+	}
+
+	@GetMapping("/{helperId}")
+	@Operation(summary = "보호자 조회")
+	public ApiResponse<?> getHelper(
+		@PathVariable String helperId
+	) {
+		return ApiResponse.success(helperService.findById(helperId), "보호자 조회 성공");
+	}
+
+	@PatchMapping("/{helperId}")
+	@Operation(summary = "보호자 정보 수정")
+	public ApiResponse<?> updateHelper(
+		@PathVariable String helperId,
+		@RequestBody HelperRequestDto helperRequestDto
+	) {
+		helperService.update(helperId, helperRequestDto);
+		return ApiResponse.success(null, "보호자 정보 수정 성공");
+	}
+
+	@DeleteMapping("/{helperId}")
+	@Operation(summary = "보호자 탈퇴")
+	public ApiResponse<?> deleteHelper(
+		@PathVariable String helperId
+	) {
+		helperService.delete(helperId);
+		return ApiResponse.success(null, "보호자 탈퇴 성공");
 	}
 
 }
--- src/main/java/com/server/slosaeng/domain/member/application/HelperService.java
+++ src/main/java/com/server/slosaeng/domain/member/application/HelperService.java
@@ -3,9 +3,11 @@ package com.server.slosaeng.domain.member.application;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.server.slosaeng.domain.login.application.RefreshTokenService;
 import com.server.slosaeng.domain.member.dao.HelperRepository;
 import com.server.slosaeng.domain.member.domain.Helper;
 import com.server.slosaeng.domain.member.dto.request.HelperRequestDto;
+import com.server.slosaeng.domain.member.dto.response.HelperResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HelperService {
 
+	private final RefreshTokenService refreshTokenService;
 	private final HelperRepository helperRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -26,4 +29,34 @@ public class HelperService {
 			.build()).getId();
 	}
 
+	public HelperResponseDto findById(String helperId) {
+		Helper helper = findHelperById(helperId);
+		return HelperResponseDto.builder()
+			.id(helper.getId())
+			.name(helper.getName())
+			.phone(helper.getPhone())
+			.idNumber(helper.getIdNumber())
+			.elderIds(helper.getElderIds())
+			.build();
+	}
+
+	public void update(String helperId, HelperRequestDto helperRequestDto) {
+		Helper helper = findHelperById(helperId);
+		helper.updatePassword(bCryptPasswordEncoder.encode(helperRequestDto.getPassword()));
+		helper.updateName(helperRequestDto.getName());
+		helper.updatePhone(helperRequestDto.getPhone());
+		helper.updateIdNumber(helperRequestDto.getIdNumber());
+		helperRepository.save(helper);
+	}
+
+	public void delete(String helperId) {
+		refreshTokenService.delete(helperId);
+		helperRepository.deleteById(helperId);
+	}
+
+	private Helper findHelperById(String helperId) {
+		return helperRepository.findById(helperId).orElseThrow(
+			() -> new IllegalArgumentException("해당 보호자가 존재하지 않습니다.")
+		);
+	}
 }

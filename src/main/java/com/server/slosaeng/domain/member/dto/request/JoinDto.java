14cb0acf3a7ab636d3073994b1ceba0ebbae745f
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 17:49:02 +0900

    feat: 회원가입 구현

--- src/main/java/com/server/slosaeng/domain/member/dto/request/JoinDto.java
+++ src/main/java/com/server/slosaeng/domain/member/dto/request/JoinDto.java
@@ -1,5 +1,7 @@
 package com.server.slosaeng.domain.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +9,16 @@ import lombok.Setter;
 @Setter
 public class JoinDto {
 
+	@NotNull
+	@Schema(description = "아이디", example = "leehaneum")
 	private String id;
+
+	@NotNull
+	@Schema(description = "비밀번호", example = "1234!")
 	private String password;
+
+	@NotNull
+	@Schema(description = "이름", example = "이한음")
 	private String name;
 
 }

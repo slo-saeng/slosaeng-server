96178781a49d8ca83953b7e4b497c54c72041a59
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-11 17:49:19 +0900

    refactor: 토큰 발급 재구현

---
+++ src/main/java/com/server/slosaeng/domain/login/api/LoginController.java
@@ -0,0 +1,43 @@
+package com.server.slosaeng.domain.login.api;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.server.slosaeng.domain.login.application.LoginService;
+import com.server.slosaeng.domain.login.dto.request.CreateAccessTokenRequest;
+import com.server.slosaeng.domain.login.dto.request.LoginRequestDto;
+import com.server.slosaeng.domain.login.dto.response.CreateAccessTokenResponse;
+import com.server.slosaeng.domain.login.dto.response.LoginResponseDto;
+import com.server.slosaeng.global.common.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@Tag(name = "auth", description = "로그인 관리")
+public class LoginController {
+	private final LoginService loginService;
+
+	@PostMapping("/login")
+	@Operation(summary = "로그인")
+	public ApiResponse<?> login(
+		@RequestBody LoginRequestDto loginRequestDto
+	) {
+		LoginResponseDto loginResponseDto = loginService.login(loginRequestDto);
+		return ApiResponse.success(loginResponseDto, "login success");
+	}
+
+	@PostMapping("/new-access-token")
+	@Operation(summary = "새로운 액세스 토큰 발급")
+	public ApiResponse<CreateAccessTokenResponse> createNewAccessToken(
+		@RequestBody CreateAccessTokenRequest request
+	) {
+		String newAccessToken = loginService.createNewAccessToken(request.getRefreshToken());
+		return ApiResponse.success(new CreateAccessTokenResponse(newAccessToken), "new access token created");
+	}
+}

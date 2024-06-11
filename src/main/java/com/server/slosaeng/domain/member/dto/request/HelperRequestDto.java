9698f3a3787b146e21a05a0d9bd3d426b00d8531
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-12 00:55:46 +0900

    feat: Helper dto 생성

---
+++ src/main/java/com/server/slosaeng/domain/member/dto/request/HelperRequestDto.java
@@ -0,0 +1,31 @@
+package com.server.slosaeng.domain.member.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelperRequestDto {
+
+	@NotNull
+	@Schema(description = "아이디", example = "leehaneum")
+	private String id;
+
+	@NotNull
+	@Schema(description = "비밀번호", example = "1234!")
+	private String password;
+
+	@NotNull
+	@Schema(description = "이름", example = "이한음")
+	private String name;
+
+	@NotNull
+	@Schema(description = "전화번호", example = "010-1234-5678")
+	private String phone;
+
+	@NotNull
+	@Schema(description = "주민번호", example = "030811-1234567")
+	private String idNumber;
+}

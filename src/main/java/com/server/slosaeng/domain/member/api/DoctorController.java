b7e764cae3d1e0357546acf58c31aad71e964b9e
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-13 00:16:26 +0900

    feat: Doctor crud api 구현

---
+++ src/main/java/com/server/slosaeng/domain/member/api/DoctorController.java
@@ -0,0 +1,62 @@
+package com.server.slosaeng.domain.member.api;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.server.slosaeng.domain.member.application.DoctorService;
+import com.server.slosaeng.domain.member.dto.request.DoctorRequestDto;
+import com.server.slosaeng.global.common.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/doctor")
+@Tag(name = "Doctor", description = "의료진 관리")
+public class DoctorController {
+	private final DoctorService doctorService;
+
+	@PostMapping("")
+	@Operation(summary = "의료진 가입")
+	public ApiResponse<?> createDoctor(
+		@RequestBody DoctorRequestDto doctorRequestDto
+	) {
+		String id = doctorService.save(doctorRequestDto);
+		return ApiResponse.success(id, "Creating doctor succeed");
+	}
+
+	@GetMapping("/{doctorId}")
+	@Operation(summary = "의료진 조회")
+	public ApiResponse<?> getDoctor(
+		@PathVariable String doctorId
+	) {
+		return ApiResponse.success(doctorService.findById(doctorId), "Reading doctor succeed");
+	}
+
+	@PatchMapping("/{doctorId}")
+	@Operation(summary = "의료진 정보 수정")
+	public ApiResponse<?> updateDoctor(
+		@PathVariable String doctorId,
+		@RequestBody DoctorRequestDto doctorRequestDto
+	) {
+		doctorService.update(doctorId, doctorRequestDto);
+		return ApiResponse.success(true, "Updating doctor succeed");
+	}
+
+	@DeleteMapping("/{doctorId}")
+	@Operation(summary = "의료진 탈퇴")
+	public ApiResponse<?> deleteDoctor(
+		@PathVariable String doctorId
+	) {
+		doctorService.delete(doctorId);
+		return ApiResponse.success(true, "Deleting doctor succeed");
+	}
+}

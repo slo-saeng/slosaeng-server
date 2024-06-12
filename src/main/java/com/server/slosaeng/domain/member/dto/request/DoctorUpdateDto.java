b794a8cb26044a3c4dab453c4ac17ee89a148584
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-13 00:16:04 +0900

    feat: Doctor dto 작성

---
+++ src/main/java/com/server/slosaeng/domain/member/dto/request/DoctorUpdateDto.java
@@ -0,0 +1,31 @@
+package com.server.slosaeng.domain.member.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DoctorUpdateDto {
+
+	@NotNull
+	@Schema(description = "비밀번호", example = "asdf!")
+	private String password;
+
+	@NotNull
+	@Schema(description = "이름", example = "김정은")
+	private String name;
+
+	@NotNull
+	@Schema(description = "전화번호", example = "010-1234-6789")
+	private String phone;
+
+	@NotNull
+	@Schema(description = "직책", example = "전공의")
+	private String position;
+
+	@NotNull
+	@Schema(description = "생년월일", example = "19990101")
+	private String birth;
+}

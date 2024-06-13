ab2cf169c9311a38aa6ab719803728967feee1c5
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-13 17:30:47 +0900

    fix: 리프레시토큰 duplicate key 버그 픽스

--- src/main/java/com/server/slosaeng/domain/login/dao/RefreshTokenRepository.java
+++ src/main/java/com/server/slosaeng/domain/login/dao/RefreshTokenRepository.java
@@ -14,4 +14,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 	Optional<RefreshToken> findByRefreshToken(String refreshToken);
 
 	void deleteByMemberId(String memberId);
+
+	boolean existsByMemberId(String memberId);
 }
